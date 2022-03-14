0345420f757bac92123db4212171dd26a1d757a1
lenzjason <lenzj@ismanila.org>
2022-03-15 05:35:32 +0800

    work on project

--- S2A2 Assessment/TicTacToe.py
+++ S2A2 Assessment/TicTacToe.py
@@ -10,10 +10,13 @@ class TicTacToe:
         self.current_window.title("TacTacToe")
         self.current_window.geometry("500x524")
 
+        self.image_x = PhotoImage(file="assets/image_x.png")
+        self.image_o = PhotoImage(file="assets/image_o.png")
+
         self.canvas = Canvas(self.current_window, width=500, height=524, borderwidth=0, highlightthickness=0)
         self.canvas.pack()
 
-        self.image_tictactoe_background = PhotoImage(file="assets/tictactoe_background.png")
+        self.image_tictactoe_background = PhotoImage(file="assets/tictactoe_background-alt.png")
         self.canvas.create_image(0, 0, image=self.image_tictactoe_background, anchor="nw")
 
         self.button_exit = Button(self.canvas, text="Exit", command=self.open_main_window)
@@ -25,6 +28,8 @@ class TicTacToe:
         self.game_board = []
         self.start()
 
+        self.show_buttons()
+
     def open_main_window(self):
         """documentation"""
         from MainWindow import MainWindow
@@ -33,30 +38,33 @@ class TicTacToe:
 
     def start(self):
         """documentation"""
-        self.game_board = [{"tile": "", "coordinates": (125, 125)}, {"tile": "", "coordinates": (250, 125)}, {"tile": "", "coordinates": (375, 125)},
-                           {"tile": "", "coordinates": (125, 250)}, {"tile": "", "coordinates": (250, 250)}, {"tile": "", "coordinates": (375, 250)},
-                           {"tile": "", "coordinates": (125, 375)}, {"tile": "", "coordinates": (250, 375)}, {"tile": "", "coordinates": (375, 375)},]
+        self.game_board = [{"tile": "", "pos_x": 84, "pos_y": 168}, {"tile": "", "pos_x": 250, "pos_y": 168}, {"tile": "", "pos_x": 416, "pos_y": 168},
+                           {"tile": "", "pos_x": 84, "pos_y": 318}, {"tile": "", "pos_x": 250, "pos_y": 318}, {"tile": "", "pos_x": 416, "pos_y": 318},
+                           {"tile": "", "pos_x": 84, "pos_y": 498}, {"tile": "", "pos_x": 250, "pos_y": 498}, {"tile": "", "pos_x": 416, "pos_y": 498}]
         self.display_board()
 
     def display_board(self):
-        self.image_x = PhotoImage(file="assets/image_x.png")
-        self.image_o = PhotoImage(file="assets/image_o.png")
-
-        for i in range(len(self.gameboard)):
-            if self.gameboard[i]["tile"] == "":
+        for i in range(len(self.game_board)):
+            if self.game_board[i]["tile"] == "":
                 # make button
-
-            elif self.gameboard[i]["tile"] == "x":
+                pass
+            elif self.game_board[i]["tile"] == "x":
                 # make x
                 pass
-            elif self.gameboard[i]["tile"] == "o":
+            elif self.game_board[i]["tile"] == "o":
                 # make o
                 pass
 
+    def show_buttons(self,):
+        """documentation"""
+        for tile in self.game_board:
+            if tile["tile"] == "":
+                Button(self.canvas, text="PLACE X", command=self.change_tile(tile)).place(x=tile["pos_x"], y=tile["pos_y"], height=50, width=100, anchor=CENTER)
 
+    def change_tile(self, tile):
+        """documentation"""
+        pass
 
-
-
-# root = Tk()
-# app = TicTacToe(root)
-# root.mainloop()
+root = Tk()
+app = TicTacToe(root)
+root.mainloop()
